3ee502c94d901d223a1384d9ca91f260ae9efbcc
areti <aretpett@gmail.com>
2023-10-31 17:35:31 +0200

    init project

---
+++ app/src/main/java/com/ap/travelrate/activity/authentication/SignInActivity.java
@@ -0,0 +1,34 @@
+package com.ap.travelrate.activity.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ap.travelrate.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private TextView signUpTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        signUpTextView = findViewById(R.id.signUpTextView);
+    }
+
+    private void setListeners() {
+        // add listener for signUpTextView to open the respective activity
+        signUpTextView.setOnClickListener(view ->
+                startActivity(new Intent(SignInActivity.this, SignUpActivity.class)));
+    }
+
+
+    public void signIn(View view) {
+
+    }
+
+}
---
+++ app/src/main/java/com/ap/travelrate/activity/authentication/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.ap.travelrate.activity.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ap.travelrate.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
