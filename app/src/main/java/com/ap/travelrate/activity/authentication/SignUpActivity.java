9d5468f471ac1f17f1016e2f125ed1faa87e5581
areti <aretpett@gmail.com>
2023-11-01 16:25:51 +0200

    added validation on sign up

--- app/src/main/java/com/ap/travelrate/activity/authentication/SignUpActivity.java
+++ app/src/main/java/com/ap/travelrate/activity/authentication/SignUpActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import com.ap.travelrate.R;
 import com.ap.travelrate.activity.authentication.domain.User;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,6 +22,10 @@ public class SignUpActivity extends AppCompatActivity {
     private Button signUpButton;
     private FirebaseAuth auth;
     private FirebaseDatabase database;
+
+    private final String usernameRgx = "^_*[a-zA-Z0-9]+(_*[a-zA-Z0-9]*)*$",
+            emailRgx = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$",
+            passwordRgx = "^[0-9]{4}$";
 
 
     @Override
@@ -35,16 +41,27 @@ public class SignUpActivity extends AppCompatActivity {
         // authentication via firebaseAuth
         auth = FirebaseAuth.getInstance();
 
-        // set listeners to controls
+        // ------ Set listeners to controls
         setListeners();
     }
 
     private void setListeners() {
-        // todo if it's needed
+        // ------ Add listener to sign Up button
         signUpButton.setOnClickListener(this::signUp);
     }
 
     public void signUp(View view) {
+        // ------ Validate inputs
+        if(!validateInputs(username.getText().toString().trim(), email.getText().toString().trim(),
+                password.getText().toString().trim(), confirmPassword.getText().toString().trim())) {
+            new AlertDialog.Builder(SignUpActivity.this)
+                    .setTitle("Error")
+                    .setMessage("Some fields are invalid or passwords don't match.")
+                    .show();
+            return;
+        }
+
+        // otherwise continue with registering the new user
         auth.createUserWithEmailAndPassword(email.getText().toString().trim(), password.toString().trim())
                 .addOnCompleteListener(task -> {
                     if(task.isSuccessful()) {
@@ -60,6 +77,14 @@ public class SignUpActivity extends AppCompatActivity {
                 });
     }
 
+    private boolean validateInputs(String username, String email, String password, String confirmPassword) {
+        return username.matches(usernameRgx) &&
+                email.matches(emailRgx) &&
+                password.matches(passwordRgx) &&
+                confirmPassword.matches(passwordRgx) &&
+                password.equals(confirmPassword);
+    }
+
     private void createUser() {
         User user = new User(username.getText().toString().trim(),
                 email.getText().toString().trim(), password.getText().toString().trim());
@@ -72,7 +97,14 @@ public class SignUpActivity extends AppCompatActivity {
             if(!task.isSuccessful()) {
                 title = "Failed";
                 message = "Failed to sign up.";
-                //todo: rollback and delete the current user
+                // rollback: remove the authenticated user from firebase users
+                FirebaseUser deleteUser = auth.getCurrentUser();
+                deleteUser.delete()
+                        .addOnCompleteListener(deleteTask -> {
+                            if(deleteTask.isSuccessful()) {
+                                Log.d("Delete User", "The user has been successfully been removed.");
+                            }
+                        });
             }
             new AlertDialog.Builder(SignUpActivity.this)
                     .setTitle(title)
